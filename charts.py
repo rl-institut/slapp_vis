e8517f9abd69addf765f95bd45a810a5062ea707
Hendrik Huyskens <hendrik.huyskens@rl-institut.de>
2025-04-08 19:52:01 +0200

    Add electricity import chart

--- charts.py
+++ charts.py
@@ -25,8 +25,19 @@ def total_electricity_per_technology():
     filtered["value"] = (filtered["value"] / filtered["value"].sum() * 100).round()
     return template, filtered.to_dict(orient="records")
 
+def electricity_import():
+    template = "electricity_import.html"
+    scalars = chart_data.get_postprocessed_data()
+    import_df = scalars[(scalars["var_name"] == "flow_out_electricity") & (scalars["tech"] == "import") & (scalars["var_value"] > 0)][["name", "var_value"]]
+    export_df = scalars[
+        (scalars["var_name"] == "flow_out_electricity") & (scalars["tech"] == "export") & (scalars["var_value"] > 0)][
+        ["name", "var_value"]]
+    result = [import_df["var_value"].sum(), -export_df["var_value"].sum()]
+    return template, result
+
+
 
 if __name__ == "__main__":
-    _template, _data = total_electricity_per_technology()
+    _template, _data = electricity_import()
     render_chart(_template, _data)
 
