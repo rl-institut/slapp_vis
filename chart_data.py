f0f04ec2407d5339307cdc1f75f73584e8b240fb
Hendrik Huyskens <hendrik.huyskens@rl-institut.de>
2025-04-15 20:19:16 +0200

    Run linter

--- chart_data.py
+++ chart_data.py
@@ -1,9 +1,6 @@
-
 import pathlib
 import pandas as pd
 import os
-import numpy as np
-
 
 
 DATA_DIR = pathlib.Path(__file__).parent / "data"
@@ -11,59 +8,69 @@ OEMOF_SCENARIO = "2045_scenario"
 
 
 def get_postprocessed_data():
-    return pd.read_csv(DATA_DIR / OEMOF_SCENARIO / "postprocessed" / "scalars.csv", delimiter=";")
+    return pd.read_csv(
+        DATA_DIR / OEMOF_SCENARIO / "postprocessed" / "scalars.csv", delimiter=";"
+    )
+
 
 def get_preprocessed_file_list():
     path = DATA_DIR / OEMOF_SCENARIO / "preprocessed" / "data" / "elements"
-    print(path)
-    file_list = [f for f in os.listdir(path) if f.endswith('.csv')]
-    filtered_file_list = [f for f in file_list if not any(x in f for x in ["commodity", "demand", "bus", "export", "import", "transmission"])]
+    file_list = [f for f in os.listdir(path) if f.endswith(".csv")]
+    filtered_file_list = [
+        f
+        for f in file_list
+        if not any(
+            x in f
+            for x in ["commodity", "demand", "bus", "export", "import", "transmission"]
+        )
+    ]
     return filtered_file_list
+
 
 def get_preprocessed_file_df():
     resultDf = pd.DataFrame()
     file_list = get_preprocessed_file_list()
 
-    for file in  file_list:
-        file_path = DATA_DIR / OEMOF_SCENARIO / "preprocessed" / "data" / "elements" / file
+    for file in file_list:
+        file_path = (
+            DATA_DIR / OEMOF_SCENARIO / "preprocessed" / "data" / "elements" / file
+        )
         techDf = pd.read_csv(file_path, delimiter=";")
         resultDf = pd.concat([resultDf, techDf], ignore_index=True)
 
     return resultDf
 
-def get_postprocessed_sequences_bus_time(): 
+
+def get_postprocessed_sequences_bus_time():
     path = DATA_DIR / OEMOF_SCENARIO / "postprocessed" / "sequences" / "bus"
-    file_list = [f for f in os.listdir(path) if f.endswith('.csv')]
-    
+    file_list = [f for f in os.listdir(path) if f.endswith(".csv")]
+
     data = pd.Series()
 
     for file_name in file_list:
-        df = pd.read_csv(path / file_name, sep=';')
+        df = pd.read_csv(path / file_name, sep=";")
         for col_name in df.columns:
             cell_name = df.iloc[0][col_name]
-            
-            if 'electricity-demand' in cell_name:
+
+            if "electricity-demand" in cell_name:
                 column_sum = df.loc[2:, col_name].astype(float).sum()
 
-                parts = file_name.split('-')
+                parts = file_name.split("-")
                 region = parts[0]
 
-                data[region] = column_sum    
+                data[region] = column_sum
 
     return data
 
-def get_postprocessed_by_variable_flow(filename = "flow.csv"):
-    path = DATA_DIR / OEMOF_SCENARIO / "postprocessed" / "sequences" / "by_variable" / filename
-    df = pd.read_csv(path, sep=';',skiprows=3, header=None, index_col=0)
+
+def get_postprocessed_by_variable_flow(filename="flow.csv"):
+    path = (
+        DATA_DIR
+        / OEMOF_SCENARIO
+        / "postprocessed"
+        / "sequences"
+        / "by_variable"
+        / filename
+    )
+    df = pd.read_csv(path, sep=";", skiprows=3, header=None, index_col=0)
     return df
-
-
-
-
-
-   
-        
-       
-
-    
-
--- charts.py
+++ charts.py
@@ -1,4 +1,3 @@
-
 import json
 import pathlib
 import string
@@ -46,51 +45,66 @@ def merge_technologies(technology):
 def total_electricity_per_technology():
     template = "total_electricity_per_technology.html"
     scalars = chart_data.get_postprocessed_data()
-    filtered = scalars[(scalars["var_name"] == "flow_out_electricity") & (scalars["var_value"] > 0)][["name", "var_value"]]
+    filtered = scalars[
+        (scalars["var_name"] == "flow_out_electricity") & (scalars["var_value"] > 0)
+    ][["name", "var_value"]]
     filtered.columns = ["name", "value"]
     filtered["value"] = (filtered["value"] / filtered["value"].sum() * 100).round()
     return template, filtered.to_dict(orient="records")
 
+
 def electricity_import():
     template = "electricity_import.html"
     scalars = chart_data.get_postprocessed_data()
-    import_df = scalars[(scalars["var_name"] == "flow_out_electricity") & (scalars["tech"] == "import") & (scalars["var_value"] > 0)][["name", "var_value"]]
+    import_df = scalars[
+        (scalars["var_name"] == "flow_out_electricity")
+        & (scalars["tech"] == "import")
+        & (scalars["var_value"] > 0)
+    ][["name", "var_value"]]
     export_df = scalars[
-        (scalars["var_name"] == "flow_in_electricity") & (scalars["tech"] == "export") & (scalars["var_value"] > 0)][
-        ["name", "var_value"]]
+        (scalars["var_name"] == "flow_in_electricity")
+        & (scalars["tech"] == "export")
+        & (scalars["var_value"] > 0)
+    ][["name", "var_value"]]
     result = [import_df["var_value"].sum(), -export_df["var_value"].sum()]
     return template, result
 
+
 def optimized_capacities():
     template = "optimized_capacities.html"
-    
+
     file_list = chart_data.get_preprocessed_file_list()
     file_names_list = [file_name[:-4] for file_name in file_list]
 
     scalars_df = chart_data.get_postprocessed_data()
 
-    var_value_df = scalars_df[(scalars_df['var_name'].str.contains('invest_out|invest_in')) &
-                        (scalars_df['name'].str.contains('|'.join(file_names_list)) ) 
-                       ]
-
+    var_value_df = scalars_df[
+        (scalars_df["var_name"].str.contains("invest_out|invest_in"))
+        & (scalars_df["name"].str.contains("|".join(file_names_list)))
+    ]
 
     capacity_potential_df = chart_data.get_preprocessed_file_df()
 
-    merged_df = pd.merge(var_value_df, capacity_potential_df, on='name', how='outer')
-    merged_df = merged_df[['name', 'var_value', 'capacity_potential', 'var_name']]
+    merged_df = pd.merge(var_value_df, capacity_potential_df, on="name", how="outer")
+    merged_df = merged_df[["name", "var_value", "capacity_potential", "var_name"]]
 
+    merged_df.loc[
+        merged_df["capacity_potential"] == float("inf"), "capacity_potential"
+    ] = 0
+    merged_df.loc[
+        merged_df["capacity_potential"] == float("-inf"), "capacity_potential"
+    ] = 0
 
-
-
-    merged_df.loc[merged_df['capacity_potential'] == float('inf'), 'capacity_potential'] = 0
-    merged_df.loc[merged_df['capacity_potential'] == float('-inf'), 'capacity_potential'] = 0
-
-    merged_df = merged_df[(merged_df['var_value'].notna()) & 
-                          (merged_df['capacity_potential'].notna() &
-                           merged_df['capacity_potential'] > 0)
-                          ]
+    merged_df = merged_df[
+        (merged_df["var_value"].notna())
+        & (
+            merged_df["capacity_potential"].notna() & merged_df["capacity_potential"]
+            > 0
+        )
+    ]
 
     return template, merged_df.to_dict(orient="records")
+
 
 def generation_consumption_per_sector():
     template = "generation_consumption_per_sector.html"
@@ -98,48 +112,119 @@ def generation_consumption_per_sector():
 
     scalars["name"] = scalars["name"].apply(merge_technologies)
 
-    y1 = scalars.loc[(scalars["var_name"] == "flow_out_electricity") & (scalars["var_value"] > 0), ["name", "var_value"]].groupby("name").sum()
-    x1 = int(scalars.loc[(scalars["var_name"] == "flow_in_electricity"), "var_value"].sum().sum() / 1000) * 1000
+    y1 = (
+        scalars.loc[
+            (scalars["var_name"] == "flow_out_electricity")
+            & (scalars["var_value"] > 0),
+            ["name", "var_value"],
+        ]
+        .groupby("name")
+        .sum()
+    )
+    x1 = (
+        int(
+            scalars.loc[(scalars["var_name"] == "flow_in_electricity"), "var_value"]
+            .sum()
+            .sum()
+            / 1000
+        )
+        * 1000
+    )
 
-    y2 = scalars.loc[(scalars["var_name"] == "flow_out_heat_low_decentral") & (scalars["var_value"] > 0), ["name", "var_value"]].groupby("name").sum()
-    x2 = int(scalars.loc[(scalars["var_name"] == "flow_in_heat_low_decentral"), "var_value"].sum().sum() / 1000) * 1000
+    y2 = (
+        scalars.loc[
+            (scalars["var_name"] == "flow_out_heat_low_decentral")
+            & (scalars["var_value"] > 0),
+            ["name", "var_value"],
+        ]
+        .groupby("name")
+        .sum()
+    )
+    x2 = (
+        int(
+            scalars.loc[
+                (scalars["var_name"] == "flow_in_heat_low_decentral"), "var_value"
+            ]
+            .sum()
+            .sum()
+            / 1000
+        )
+        * 1000
+    )
 
-    y3 = scalars.loc[(scalars["var_name"] == "flow_out_heat_low_central") & (scalars["var_value"] > 0), ["name", "var_value"]].groupby("name").sum()
-    x3 = int(scalars.loc[(scalars["var_name"] == "flow_in_heat_low_central"), "var_value"].sum().sum() / 1000) * 1000
+    y3 = (
+        scalars.loc[
+            (scalars["var_name"] == "flow_out_heat_low_central")
+            & (scalars["var_value"] > 0),
+            ["name", "var_value"],
+        ]
+        .groupby("name")
+        .sum()
+    )
+    x3 = (
+        int(
+            scalars.loc[
+                (scalars["var_name"] == "flow_in_heat_low_central"), "var_value"
+            ]
+            .sum()
+            .sum()
+            / 1000
+        )
+        * 1000
+    )
 
-    y4 = scalars.loc[(scalars["var_name"] == "flow_out_heat_high") & (scalars["var_value"] > 0), ["name", "var_value"]].groupby("name").sum()
-    x4 = int(scalars.loc[(scalars["var_name"] == "flow_in_heat_high"), "var_value"].sum().sum() / 1000) * 1000
+    y4 = (
+        scalars.loc[
+            (scalars["var_name"] == "flow_out_heat_high") & (scalars["var_value"] > 0),
+            ["name", "var_value"],
+        ]
+        .groupby("name")
+        .sum()
+    )
+    x4 = (
+        int(
+            scalars.loc[(scalars["var_name"] == "flow_in_heat_high"), "var_value"]
+            .sum()
+            .sum()
+            / 1000
+        )
+        * 1000
+    )
 
     data = {
         "chart1-1": (y1 / y1.sum() * 100).to_dict()["var_value"],
         "chart1-total": x1,
-
         "chart2-1": y2.to_dict()["var_value"],
         "chart2-total": x2,
-
         "chart3-1": y3.to_dict()["var_value"],
         "chart3-total": x3,
-
         "chart4-1": y4.to_dict()["var_value"],
         "chart4-total": x4,
     }
     return template, data
 
+
 def self_generation_imports():
     template = "self_generation_power.html"
     scalars = chart_data.get_postprocessed_data()
-    y1_df = scalars[(scalars['var_name'] == 'flow_out_electricity') & (~scalars['type'].isin(['shortage', 'storage']))]
-    y1 = y1_df['var_value'].sum()
+    y1_df = scalars[
+        (scalars["var_name"] == "flow_out_electricity")
+        & (~scalars["type"].isin(["shortage", "storage"]))
+    ]
+    y1 = y1_df["var_value"].sum()
 
-    y2_df = scalars[(scalars['var_name'] == 'flow_out_electricity') & (scalars['tech'] == 'import')]
-    y2 = y2_df['var_value'].sum()
-    
+    y2_df = scalars[
+        (scalars["var_name"] == "flow_out_electricity") & (scalars["tech"] == "import")
+    ]
+    y2 = y2_df["var_value"].sum()
+
     data = {
         "y1": y1,
         "y2": y2,
         "x": round(y1 + y2, 2),
     }
     return template, data
+
 
 def supplied_hours():
     template = "supplied_hours.html"
@@ -151,24 +236,25 @@ def supplied_hours():
     y2 = y2 / x * 100
 
     data = pd.DataFrame()
-    data['y1'] = y1.round(2)
-    data['y2'] = y2.round(2)
-
-    print(data.to_dict())
+    data["y1"] = y1.round(2)
+    data["y2"] = y2.round(2)
 
     return template, data.to_dict()
+
 
 def interactive_time_series_plot():
     template = "interactive_time_series_plot.html"
     data_df = pd.DataFrame()
     flow_df = chart_data.get_postprocessed_by_variable_flow("flow.csv")
-    storage_content_df = chart_data.get_postprocessed_by_variable_flow("storage_content.csv")
+    storage_content_df = chart_data.get_postprocessed_by_variable_flow(
+        "storage_content.csv"
+    )
 
-    data_df['flow zeit 1'] = flow_df[1]
-    data_df['storage content zeit 1'] = storage_content_df[1]
+    data_df["flow zeit 1"] = flow_df[1]
+    data_df["storage content zeit 1"] = storage_content_df[1]
 
-    data_df['flow zeit N'] = flow_df[2]
-    data_df['storage content zeit N'] = storage_content_df[3]
+    data_df["flow zeit N"] = flow_df[2]
+    data_df["storage content zeit N"] = storage_content_df[3]
     return template, data_df.to_dict()
 
 
@@ -179,4 +265,3 @@ if __name__ == "__main__":
             continue
         _template, _data = fct()
         render_chart(_template, _data)
-
